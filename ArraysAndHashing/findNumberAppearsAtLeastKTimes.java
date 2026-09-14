8caefeb3e6b6de5a82fd24c6572b470959784e31
Tram Anh Nguyen <157590309+alitanguyen@users.noreply.github.com>
2026-09-15 00:59:47 +0100

    Add best time to buy and sell stock

--- ArraysAndHashing/findNumberAppearsAtLeastKTimes.java
+++ ArraysAndHashing/findNumberAppearsAtLeastKTimes.java
@@ -3,37 +3,59 @@
 // Output: [2, 3, 4]
 
 package ArraysAndHashing;
-
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class findNumberAppearsAtLeastKTimes {
+
+    // Option 1: 
+    // public int[] findNumberAppearTimes(int[] nums, int k) {
+
+    //     // count frequencies 
+    //     Map<Integer, Integer> freq = new HashMap<>();
+
+    //     for (int num : nums) {
+    //         freq.put(num, freq.getOrDefault(num, 0) + 1);
+    //     }
+
+    //     // find numbers appearing at least k times
+    //     ArrayList<Integer> resultList = new ArrayList<>();
+    //     for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+    //         if (entry.getValue() >= k) {
+    //             resultList.add(entry.getKey());
+    //         }
+    //     }
+
+    //     // convert ArrayList<Integer> to int[]
+    //     int[] result = new int[resultList.size()];
+    //     for (int i = 0; i < resultList.size(); i++) {
+    //         result[i] = resultList.get(i);
+    //     }
+    //     return result;
+    // }
+
+    // Option 2: 
     public int[] findNumberAppearTimes(int[] nums, int k) {
-
-        // count frequencies 
-        Map<Integer, Integer> freq = new HashMap<>();
-
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        if (k > nums.length) {
+            return new int[0];
         }
 
-        // find numbers appearing at least k times
-        ArrayList<Integer> resultList = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
-            if (entry.getValue() >= k) {
-                resultList.add(entry.getKey());
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        Set<Integer> result = new HashSet<>();
+
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+            if (freqMap.get(num) >= k) {
+                result.add(num);
             }
         }
 
-        // convert ArrayList<Integer> to int[]
-        int[] result = new int[resultList.size()];
-        for (int i = 0; i < resultList.size(); i++) {
-            result[i] = resultList.get(i);
-        }
-        return result;
-    }
+        // covert to primitive/native int[]
+        return result.stream().mapToInt(Integer::intValue).toArray();
 
+    }
     public int[] returnTopKFrequent(int[] nums, int k) {
         return findNumberAppearTimes(nums, k);
     }
