66b4c37c2bbee9a1db5660e84720de13428b02f9
Tram Anh Nguyen <157590309+alitanguyen@users.noreply.github.com>
2026-09-15 09:47:24 +0100

    rename folders and files

---
+++ two_pointers/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,25 @@
+package TwoPointers;
+
+public class bestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+            } else {
+                left = right;
+            }
+            right++;
+        }
+
+        return maxProfit;
+    }
+}
+
